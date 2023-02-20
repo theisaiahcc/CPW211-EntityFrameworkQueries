9ba9b79549db054e09e2824162a77c3d3372d6a1
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-02-20 14:58:20 -0800

    Select specific columns

--- CPW211-EntityFrameworkQueries/Form1.Designer.cs
+++ CPW211-EntityFrameworkQueries/Form1.Designer.cs
@@ -30,14 +30,14 @@
         {
             this.btnSelectAllVendors = new System.Windows.Forms.Button();
             this.btnAllCaliVendors = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnSelectSpecificColumns = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSelectAllVendors
             // 
             this.btnSelectAllVendors.Location = new System.Drawing.Point(49, 42);
             this.btnSelectAllVendors.Name = "btnSelectAllVendors";
-            this.btnSelectAllVendors.Size = new System.Drawing.Size(169, 23);
+            this.btnSelectAllVendors.Size = new System.Drawing.Size(199, 23);
             this.btnSelectAllVendors.TabIndex = 0;
             this.btnSelectAllVendors.Text = "Select All From Vendors";
             this.btnSelectAllVendors.UseVisualStyleBackColor = true;
@@ -47,27 +47,28 @@
             // 
             this.btnAllCaliVendors.Location = new System.Drawing.Point(49, 103);
             this.btnAllCaliVendors.Name = "btnAllCaliVendors";
-            this.btnAllCaliVendors.Size = new System.Drawing.Size(169, 23);
+            this.btnAllCaliVendors.Size = new System.Drawing.Size(199, 23);
             this.btnAllCaliVendors.TabIndex = 1;
             this.btnAllCaliVendors.Text = "Select All Cali Vendors";
             this.btnAllCaliVendors.UseVisualStyleBackColor = true;
             this.btnAllCaliVendors.Click += new System.EventHandler(this.btnAllCaliVendors_Click);
             // 
-            // button3
+            // btnSelectSpecificColumns
             // 
-            this.button3.Location = new System.Drawing.Point(49, 164);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnSelectSpecificColumns.Location = new System.Drawing.Point(49, 164);
+            this.btnSelectSpecificColumns.Name = "btnSelectSpecificColumns";
+            this.btnSelectSpecificColumns.Size = new System.Drawing.Size(199, 23);
+            this.btnSelectSpecificColumns.TabIndex = 2;
+            this.btnSelectSpecificColumns.Text = "Select All by Name, City, and State";
+            this.btnSelectSpecificColumns.UseVisualStyleBackColor = true;
+            this.btnSelectSpecificColumns.Click += new System.EventHandler(this.btnSelectSpecificColumns_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnSelectSpecificColumns);
             this.Controls.Add(this.btnAllCaliVendors);
             this.Controls.Add(this.btnSelectAllVendors);
             this.Name = "Form1";
@@ -80,6 +81,6 @@
 
         private Button btnSelectAllVendors;
         private Button btnAllCaliVendors;
-        private Button button3;
+        private Button btnSelectSpecificColumns;
     }
 }
--- CPW211-EntityFrameworkQueries/Form1.cs
+++ CPW211-EntityFrameworkQueries/Form1.cs
@@ -1,5 +1,7 @@
 using CPW211_EntityFrameworkQueries.Models;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
+using System.Text;
 
 namespace CPW211_EntityFrameworkQueries
 {
@@ -26,5 +28,33 @@ namespace CPW211_EntityFrameworkQueries
                                             .OrderBy(v => v.VendorState)
                                             .ToList();
         }
+
+        private void btnSelectSpecificColumns_Click(object sender, EventArgs e)
+        {
+            using ApContext dbContext = new ApContext();
+
+            // anonymous type
+            List<VendorLocation> results = (from v in dbContext.Vendors
+                          select new VendorLocation
+                          { 
+                              VendorName = v.VendorName,
+                              VendorState = v.VendorState, 
+                              VendorCity = v.VendorCity 
+                          }).ToList();
+            
+            StringBuilder displayString = new StringBuilder();
+            foreach ( var v in results )
+            {
+                displayString.AppendLine($"{v.VendorName} is in {v.VendorCity}");
+            }
+
+            MessageBox.Show(displayString.ToString());
+        }
     }
+
+    class VendorLocation
+    {
+        public string VendorName { get; set;}
+        public string VendorState { get; set;}
+        public string VendorCity { get; set;}
 }
